ea7b1eb7a306231f3a4a08ec327a97c758ec2492
Tuan Ngo <tuan.23a5@gmail.com>
2020-07-15 17:24:38 +0700

    fix dependencies

--- src/main/java/application/security/jwt/JwtUtils.java
+++ src/main/java/application/security/jwt/JwtUtils.java
@@ -2,6 +2,7 @@ package application.security.jwt;
 
 import application.security.jwtservice.UserDetailsImpl;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.impl.TextCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
